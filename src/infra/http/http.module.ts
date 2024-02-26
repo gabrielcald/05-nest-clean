634a8cafc2203f35e1773870a71abc2b4d0bae95
gabrielcald <gcalderaro93@gmail.com>
2024-02-26 14:29:46 -0300

    MOD 2 - Listando perguntas recentes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,6 +5,7 @@ import { CreateQuestionsController } from './controllers/create-question.control
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { DatabaseModule } from '../database/database.module'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 @Module({
   imports: [DatabaseModule],
@@ -14,6 +15,6 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
     CreateQuestionsController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
